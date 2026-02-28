90a67b524ef1c617d949670c69ecf1b0654c15a5
Belly433 <bellydynella.sabushi@final.edu.tr>
2026-02-28 19:16:42 +0200

    Add Two Sum solution

---
+++ easy/001_two_sum.dart
@@ -0,0 +1,13 @@
+void main() {
+  List<int> nums = [2, 7, 11, 15];
+  int target = 9;
+
+  for (int i = 0; i < nums.length; i++) {
+    for (int j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        print([i, j]);
+        return;
+      }
+    }
+  }
+}
